0509da207699d078a364becb8f57d10972435e92
elleiell <elleiellb@gmail.com>
2025-04-22 16:20:21 +0800

    revert package json

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
 import GroceryChecklist from './pages/GroceryChecklist';
-import Counter from './pages/Counter';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Auth />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/checklist" element={<GroceryChecklist />} />
-        <Route path="/counter" element={<Counter />} />
       </Routes>
     </Router>
   );
